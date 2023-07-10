9320a69f6cc415db1a85c610578dc2c7daef383d
loo <loo@loo.local>
2023-07-10 13:27:09 +0800

    add device info collection

--- PartnerBoost.framework/Headers/PBLinkService.h
+++ PartnerBoost.framework/Headers/PBLinkService.h
@@ -16,13 +16,15 @@ NS_ASSUME_NONNULL_BEGIN
 + (void)initWith:(NSString *)appkey idfa:(nullable NSString *)idfa;
 
 /// 根据不同的URL路由到不同的app展示页
-+ (void)routePBLink:(NSURL *)url;
++ (BOOL)routePBLink:(NSURL *)url;
 
 /// 根据universal link路由到不同的app展示页
-+ (void)continueUserActivity:(NSUserActivity *)userActivity;
++ (BOOL)continueUserActivity:(NSUserActivity *)userActivity;
 
 /// 注册一个通用的获取参数的回调
 + (void)registerHandler:(void(^_Nonnull)(PBLinkResponse *__nullable respone))handler;
+
++ (void)testPringDeviceInfo;
 
 @end
 
